3e0acc58809d5b0693302ff26edb51c4fa50ae4c
AbylAlexAnonim666 <alexdonarom@gmail.com>
2024-07-31 03:29:08 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function openTab(tabName) {
         }
     }
 }
+
+function openTab(tabName) {
+    document.getElementById("tab-iframe").src = tabName;
+    var tablinks = document.getElementsByClassName("header-button");
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    var btns = document.getElementsByClassName("header-button");
+    for (var i = 0; i < btns.length; i++) {
+        if (btns[i].getAttribute("onclick").includes(tabName)) {
+            btns[i].classList.add("active");
+        }
+    }
+}
