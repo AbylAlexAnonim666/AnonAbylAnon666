db125401017d3701b868f4c9f61eadeafc0f48ba
AbylAlexAnonim666 <alexdonarom@gmail.com>
2024-07-31 21:31:51 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -14,6 +14,20 @@ function openTab(tabName) {
     }
 }
 
+function openGameEV1(tabName) {
+    document.getElementById("tab-iframe").src = tabName;
+    var tablinks = document.getElementsByClassName("tab");
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    var btns = document.getElementsByClassName("tab");
+    for (var i = 0; i < btns.length; i++) {
+        if (btns[i].getAttribute("onclick").includes(tabName)) {
+            btns[i].classList.add("active");
+        }
+    }
+}
+
 function openHead(headName) {
     var iframe = document.getElementById("header-iframe");
     var headlinks = document.getElementsByClassName("header-button");
