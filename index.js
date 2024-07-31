3da8a91b972cc0e9e523144e87da5875490e9b7a
AbylAlexAnonim666 <alexdonarom@gmail.com>
2024-07-31 03:40:11 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ function openTab(tabName) {
     }
 }
 
-function openTab(tabName) {
-    document.getElementById("tab-iframe").src = tabName;
-    var tablinks = document.getElementsByClassName("header-button");
-    for (var i = 0; i < tablinks.length; i++) {
-        tablinks[i].classList.remove("active");
+function openHead(headName) {
+    document.getElementById("header-iframe").src = headName;
+    var headlinks = document.getElementsByClassName("header-button");
+    for (var i = 0; i < headlinks.length; i++) {
+        headlinks[i].classList.remove("active");
     }
-    var btns = document.getElementsByClassName("header-button");
-    for (var i = 0; i < btns.length; i++) {
-        if (btns[i].getAttribute("onclick").includes(tabName)) {
-            btns[i].classList.add("active");
+    var btns1 = document.getElementsByClassName("header-button");
+    for (var i = 0; i < btns1.length; i++) {
+        if (btns1[i].getAttribute("onclick").includes(headName)) {
+            btns1[i].classList.add("active");
         }
     }
 }
